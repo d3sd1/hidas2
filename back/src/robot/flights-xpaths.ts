10b25909b3d150088da060035bca2ce673f6d2be
andrei <Your Email>
2024-04-12 04:31:39 +0200

    SAGA-pattern inside. lacks on memory

---
+++ back/src/robot/flights-xpaths.ts
@@ -0,0 +1,22 @@
+export const acceptTermsXpath = 'xpath/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button';
+export const exploreXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button';
+export const resultsXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/main/div/div[2]/div/ol/li[1]';
+export const oneWaySelectorXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div/div[1]/div';
+export const oneWayXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div/div[2]/ul/li[2]';
+export const fromXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div/div/input';
+export const selectFromXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/div[2]/div[3]/ul/li[1]';
+export const toFieldXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div[1]/div/div/input';
+
+export const selectToXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div[2]/div[3]/ul/li[1]';
+export const viewFlightsBtn = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[2]/div/div[4]/div[1]/div/div[2]/div[4]/div/div/div/a';
+export const flightDateXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[2]/div[1]/div/div[1]/label[1]/span[1]/div';
+export const offerBannerBtnCloseXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[2]/div[2]/div/div/div[1]/span/span/span[2]/div/div/div/div[3]';
+
+export const orderByXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[3]/div/div/div/div[2]/div/ul/li[2]';
+export const orderBySelectorXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[3]/div/div/div/div[1]/div/button';
+export const currentResultXpath = `xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[4]/ul/li[$I_COUNTER]`;
+
+export const companyNameXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[2]/div[2]`;
+export const timeToXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[2]/div[1]/span/span[2]/span/span/span`;
+export const timeFromXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[2]/div[1]/span/span[1]/span/span/span`;
+export const priceXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[6]/div[1]/div[2]/span`;
--- back/src/robot/flights.ts
+++ back/src/robot/flights.ts
@@ -1,12 +1,21 @@
+import {
+    acceptTermsXpath, companyNameXpath,
+    currentResultXpath, exploreXpath,
+    flightDateXpath, fromXpath,
+    offerBannerBtnCloseXpath, oneWaySelectorXpath, oneWayXpath,
+    orderBySelectorXpath, orderByXpath, priceXpath, resultsXpath, selectFromXpath,
+    selectToXpath, timeFromXpath, timeToXpath,
+    toFieldXpath,
+    viewFlightsBtn
+} from './flights-xpaths';
+
 const puppeteer = require('puppeteer');
 
+// xparths
+const initialNavigation = async (headless: boolean = true) =>  {
 
-const getFlightsFromTo = async (from: string, to: string, headless: boolean = true, limit: number = 5) => {
-    const results = [];
     console.log("Generating pupeeter session!")
     const browser = await puppeteer.launch({headless, timeout: 5000, args: ['--lang=en-US,en']});
-
-
     //note: with a window resize, flight details might fail, so keep current specs.
     const page = await browser.newPage();
     // resize to 1920 x 1080
@@ -14,128 +23,133 @@ const getFlightsFromTo = async (from: string, to: string, headless: boolean = tr
 
     console.log("Initial navigation. Going directly to \"explore\" instead of \"travels\" so we reduce 1 step per each scraping.");
     await page.goto('https://www.google.com/travel/flights');
-
+    return {browser,page};
+}
+const acceptTerms = async (page: any) =>  {
     console.log("Click \"accept terms\"");
-    const acceptTermsXpath = 'xpath/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button';
     await page.waitForSelector(acceptTermsXpath);
     await page.click(acceptTermsXpath);
-
+}
+const explore = async (page: any) =>  {
     console.log("Click \"explore\"");
-    const exploreXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button';
     await page.waitForSelector(exploreXpath);
     await page.click(exploreXpath);
-
     console.log('Wait page to load results');
-    const resultsXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/main/div/div[2]/div/ol/li[1]';
     await page.waitForSelector(resultsXpath);
-
+}
+const selectOneWay = async (page: any) =>  {
     console.log("Select solo ida / one-way")
-    const oneWaySelectorXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div/div[1]/div';
     await page.waitForSelector(oneWaySelectorXpath);
     await page.click(oneWaySelectorXpath);
 
-    const oneWayXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div/div/div[2]/ul/li[2]';
     await page.waitForSelector(oneWayXpath);
     await page.click(oneWayXpath);
+}
 
 
+const pickFrom = async (page: any, from: string) =>  {
     console.log("Fill 'from' field");
-    const fromXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div/div/input';
     await page.waitForSelector(fromXpath);
     await (await page.$(fromXpath)).evaluate((input: any) => input.value = '');
     await page.type(fromXpath, from, {delay: 20});
 
-    const selectFromXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/div[2]/div[3]/ul/li[1]';
     await page.waitForSelector(selectFromXpath);
     await page.click(selectFromXpath);
 
     console.log('Wait page to load results after picking from...');
     await page.waitForSelector(resultsXpath);
 
+}
+
+const pickTo = async (page: any, to: string) =>  {
     console.log('Enter "to" field')
-    const toFieldXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div[1]/div/div/input';
     await page.waitForSelector(toFieldXpath);
     await (await page.$(toFieldXpath)).evaluate((input: any) => input.value = '');
     await page.type(toFieldXpath, to, {delay: 20});
 
-    const selectToXpath = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[1]/div[1]/section/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div[2]/div[3]/ul/li[1]';
     await page.waitForSelector(selectToXpath);
     await page.click(selectToXpath);
+}
 
+const redirectAfterSelection = async (page: any) =>  {
     console.log('check it was a valid trip and from / to are valid fields:');
     console.log('extract "view flights" url and redirect current page')
-    const viewFlightsBtn = 'xpath/html/body/c-wiz[3]/div/div[2]/div/c-wiz/div[2]/div/div/div[2]/div/div[4]/div[1]/div/div[2]/div[4]/div/div/div/a';
     try {
-        await page.waitForSelector(viewFlightsBtn, {timeout: 3000});
+        // await page.waitForSelector(viewFlightsBtn, {timeout: 3000});
     } catch (e) {
+        console.error(e)
         throw new Error('Field "from" or "to" is invalid.');
     }
     const finalFlightsUri = await page.$eval(viewFlightsBtn, (element: any) => element.getAttribute("href"));
-
     await page.goto(`https://www.google.com${finalFlightsUri}`);
+}
 
-
+const getFlightDate = async (page: any) =>  {
     console.log('Get flight date');
-    const flightDateXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[2]/div[1]/div/div[1]/label[1]/span[1]/div';
     await page.waitForSelector(flightDateXpath);
-    const flightDate = (await (await (await page.$(flightDateXpath)).getProperty('textContent')).jsonValue());
+    return (await (await (await page.$(flightDateXpath)).getProperty('textContent')).jsonValue());
+}
 
+const standarizeAndFilter = async (page: any) =>  {
     console.log('Standarize output results by selecting "all results - price" in the dropdown instead "best results"');
-    const orderBySelectorXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[3]/div/div/div/div[1]/div/button';
     await page.waitForSelector(orderBySelectorXpath);
-
     console.log('remove offer if exists');
     try {
-        const offerBannerBtnCloseXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[2]/div[2]/div/div/div[1]/span/span/span[2]/div/div/div/div[3]';
         await page.click(offerBannerBtnCloseXpath);
     } catch (e) {
 
     }
-
     console.log('select filters')
     await page.click(orderBySelectorXpath);
-
-    const orderByXpath = 'xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[3]/div/div/div/div[2]/div/ul/li[2]';
     await page.waitForSelector(orderByXpath);
     await page.click(orderByXpath);
-
 
     console.log('wait for results to be ordered... (by above spinner). ist must appear and then dissapear')
     await page.waitForSelector('.Jwkq3b.IrPQJb');
     await page.waitForSelector('.Jwkq3b.IrPQJb', {hidden: true});
+}
 
+const analyzeFlight = async (page: any, flightDate: any, i: number) =>  {
+    const companyInfoDto = {company: '', price: '', beginFlight: '', endFlight: ''};
+    //todo this should iterate results
 
-//
-    // First results: Most suitable 3
+    await page.waitForSelector(currentResultXpath.replace("$I_COUNTER", i.toString()));
+
+    console.log("Company name")
+    await page.waitForSelector(companyNameXpath.replace("$I_COUNTER", i.toString()));
+
+    console.log("Price")
+    await page.waitForSelector(priceXpath.replace("$I_COUNTER", i.toString()));
+
+    console.log("times")
+    await page.waitForSelector(timeFromXpath.replace("$I_COUNTER", i.toString()));
+
+    await page.waitForSelector(timeToXpath.replace("$I_COUNTER", i.toString()));
+
+    companyInfoDto.company = await (await (await page.$(companyNameXpath.replace("$I_COUNTER", i.toString()))).getProperty('textContent')).jsonValue()
+    companyInfoDto.price = await (await (await page.$(priceXpath.replace("$I_COUNTER", i.toString()))).getProperty('textContent')).jsonValue()
+    companyInfoDto.beginFlight = `${flightDate} - ${await (await (await page.$(timeFromXpath.replace("$I_COUNTER", i.toString()))).getProperty('textContent')).jsonValue()}`;
+    companyInfoDto.endFlight = `${flightDate} - ${await (await (await page.$(timeToXpath.replace("$I_COUNTER", i.toString()))).getProperty('textContent')).jsonValue()}`;
+
+    console.log(`Flights information:`, companyInfoDto);
+}
+
+const getFlightsFromTo = async (from: string, to: string, headless: boolean = true, limit: number = 5) => {
+    const results = [];
+
+    const {browser,page} = await initialNavigation();
+    await acceptTerms(page);
+    await explore(page);
+    await selectOneWay(page);
+    await pickFrom(page, from);
+    await pickTo(page, from);
+    await redirectAfterSelection(page);
+    const flightDate = await getFlightDate(page);
+    await standarizeAndFilter(page);
+
     for (let i = 1; i <= limit; i++) {
         try {
-            const companyInfoDto = {company: '', price: '', beginFlight: '', endFlight: ''};
-            //todo this should iterate results
-            const currentResultXpath = `xpath/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[4]/ul/li[${i}]`;
-
-            await page.waitForSelector(currentResultXpath);
-
-            console.log("Company name")
-            const companyNameXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[2]/div[2]`;
-            await page.waitForSelector(companyNameXpath);
-
-            console.log("Price")
-            const priceXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[6]/div[1]/div[2]/span`;
-            await page.waitForSelector(priceXpath);
-
-            console.log("times")
-            const timeFromXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[2]/div[1]/span/span[1]/span/span/span`;
-            await page.waitForSelector(timeFromXpath);
-
-            const timeToXpath = `${currentResultXpath}/div/div[2]/div/div[2]/div/div[2]/div[1]/span/span[2]/span/span/span`;
-            await page.waitForSelector(timeToXpath);
-
-            companyInfoDto.company = await (await (await page.$(companyNameXpath)).getProperty('textContent')).jsonValue()
-            companyInfoDto.price = await (await (await page.$(priceXpath)).getProperty('textContent')).jsonValue()
-            companyInfoDto.beginFlight = `${flightDate} - ${await (await (await page.$(timeFromXpath)).getProperty('textContent')).jsonValue()}`;
-            companyInfoDto.endFlight = `${flightDate} - ${await (await (await page.$(timeToXpath)).getProperty('textContent')).jsonValue()}`;
-
-            console.log(`Flights information:`, companyInfoDto);
+            const companyInfoDto = await analyzeFlight(page, flightDate, i);
             results.push(companyInfoDto);
         } catch (e) {
             console.log("Flight not available.", e);
@@ -143,7 +157,6 @@ const getFlightsFromTo = async (from: string, to: string, headless: boolean = tr
     }
 
     await browser.close();
-
     return results;
 }
 export default getFlightsFromTo;
